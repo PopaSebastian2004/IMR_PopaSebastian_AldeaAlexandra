b6a562766452d90e7e30a3632e304b055bd89d87
e <alexandra.aldea2004@gmail.com>
2025-10-05 16:41:59 +0300

    aAaa

---
+++ Tema1_IMR/My project (1)/Assets/CactusProximity.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class CactusProximity : MonoBehaviour
+{
+    public Animator animator;
+    public string cactusTag = "Cactus";
+    public float triggerDistance = 0.3f;
+
+    private bool wasClose = false;
+
+    void Update()
+    {
+        GameObject[] allCacti = GameObject.FindGameObjectsWithTag(cactusTag);
+        bool foundClose = false;
+
+        foreach (GameObject other in allCacti)
+        {
+            if (other == this.gameObject) continue;
+
+            float dist = Vector3.Distance(transform.position, other.transform.position);
+            if (dist < triggerDistance)
+            {
+                foundClose = true;
+                break;
+            }
+        }
+
+        if (foundClose && !wasClose)
+        {
+            animator.ResetTrigger("Attack"); 
+            animator.SetTrigger("Attack");
+        }
+
+        wasClose = foundClose;
+    }
+}
